589c3bc23277c81d0f5335b4ad151fd545e81aaf
Adam Macedo Malos <adamdomingosmm@gmail.com>
2023-04-06 15:05:52 -0300

    working on

--- js/string.js
+++
@@ -1,9 +0,0 @@
-    const navEL = document.querySelector('.navbar');
-    console.log(document.getElementsByClassName('.navbar'))
-    window.addEventListener('scroll', () => {
-        if (window.scrollY >= 56) {
-            navEL.classList.add('navbar-scrolled')
-        } else if (window.scrollY < 56) {
-            navEL.classList.remove('navbar-scrolled');
-        }
-    });
